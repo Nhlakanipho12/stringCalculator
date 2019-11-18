c6d662954a74c8e7e332b6cd21b3e9bb1aa66c89
Thulani Khoza <mavasetn@gmail.com>
2019-11-18 14:33:57 +0200

    initial commit

---
+++ spec/stringCalculator.spec.js
@@ -0,0 +1,51 @@
+const calculator = require("../src/stringCalculator");
+const stringCalculator = new calculator;
+
+describe('stringCalculator', function () {
+
+    it('should return 0 for empty input', function () {
+      expect(stringCalculator.add("")).toEqual(0);
+    });
+  
+    it('should return a number for single number input', function () {
+      expect(stringCalculator.add("1")).toEqual(1);
+    });
+  
+    it('should add two numbers separated by delimiter', function () {
+      expect(stringCalculator.add("1,2")).toEqual(3);
+    });
+  
+    it('should add many numbers separated by delimiter', function () {
+      expect(stringCalculator.add("1,2,3")).toEqual(6);
+    });
+  
+    it('should treat new line character as delimiter', function () {
+      expect(stringCalculator.add("1\n2,3")).toEqual(6);
+    });
+  
+    // it('should support custom delimiters', function () {
+    //   stringCalculator.add    });
+  
+    it('should ignore negative numbers and throw an error', function () {
+      expect(function () {
+        return stringCalculator.add("1,-2,4");
+      }).toThrowError(Error);
+    });
+  
+    it('should ignore numbers more than 1000', function () {
+      expect(stringCalculator.add("1,1001,3")).toEqual(4);
+    });
+  
+    it('should support multiple length delimiter', function () {
+      expect(stringCalculator.add("//[**]\n1**2**3")).toEqual(6);
+    });
+  
+    it('should support multiple delimiters', function () {
+      expect(stringCalculator.add("//[*][?]\n1*2?3")).toEqual(6);
+    });
+  
+  
+    it('should support number as a delimiter', function () {
+      expect(stringCalculator.add("//5\n15253")).toEqual(6);
+    });
+  });
---
+++ src/stringCalculator.js
@@ -0,0 +1,65 @@
+class stringCalculator {
+    add(stringNumber){
+      if (stringNumber === ''){
+        return 0;
+      }
+      const delimiter = this.getDelimiter(stringNumber)
+      const formattedInput = this.formatInput(stringNumber)
+      return this.calculateSum(this.getNumbers(formattedInput, delimiter))
+    }
+
+    formatInput(input){
+      const delimiterRegExp = /^(\/\/.*\n)/
+      const matches = delimiterRegExp.exec(input)
+      if(matches && matches.length > 0){
+        return input.replace(delimiterRegExp,'')
+      }
+      return input;
+    }
+  
+    getDelimiter(input){
+      const delimiters = [];
+      const multipleDelimiterRegexp = /(?:^\/\/)?\[([^\[\]]+)\]\n?/gm; /** /(?:^\/\/)?\[([^\[\]]+)\]\n?/g */
+      let matches = multipleDelimiterRegexp.exec(input);
+      while(matches !== null){
+        delimiters.push(matches[1]);
+        matches = multipleDelimiterRegexp.exec(input)
+      }
+      if(delimiters.length > 0){
+        return new RegExp('['+delimiters.join('|')+']');
+      }
+      matches = /^\/\/(.*)\n/.exec(input)
+      if(matches && matches[1]){
+        return matches[1];
+      }
+      return /[\n,]/;
+      
+    }
+
+    getNumbers(string, delimiter){
+      return string.split(delimiter)
+        .filter(n => n !== '')
+        .map(n => parseInt(n))
+    }
+  
+  calculateSum(numbers){
+    const isNagative = []
+      const finalSum = numbers.reduce((sum, n) =>{
+        if(n > 1000){
+          return 0;
+        }
+        if(n < 0){
+          isNagative.push(n)
+          return 0;
+        }
+        return sum + n;
+      },0)
+      if(isNagative.length > 0){
+        throw new Error('Negative numbers are not allowed: '+isNagative.join(','))
+      }
+      return finalSum;
+  }
+}
+  
+  
+module.exports = stringCalculator;
